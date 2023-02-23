e320f5ba3847c9aeca50d51c6a22ee8edaf72a22
Ankit <ankitlall@Ankits-MacBook-Pro.local>
2023-02-23 15:22:11 -0500

    workflow and bash

--- webapp.sh
+++ webapp.sh
@@ -2,12 +2,12 @@
 
 sudo yum update -y
 
-export DBHOST="$DBHOST"
-export DBUSER="$DBUSER"
-export DBPASS="$DBPASS"
-export DATABASE="$DATABASE"
-export PORT=$PORT
-export DBPORT=$DBPORT
+export DBHOST=${DBHOST}
+export DBUSER=${DBUSER}
+export DBPASS=${DBPASS}
+export DATABASE=${DATABASE}
+export PORT=${PORT}
+export DBPORT=${DBPORT}
 
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
@@ -23,10 +23,10 @@ unzip webapp.zip -d webapp
 cd /home/ec2-user/webapp
 npm i
 
-# #Giving exec writes to owner, user and group
-# # chmod -R 755 node_modules/
-# # rm -rf node_modules/
-# # npm i
+#Giving exec writes to owner, user and group
+# chmod -R 755 node_modules/
+# rm -rf node_modules/
+# npm i
 
 sudo cp ./webapp.service /etc/systemd/system/
 
