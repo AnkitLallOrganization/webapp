21057a465248333f139e8ca79b5440eb095ce868
Ankit <ankitlall@Ankits-MacBook-Pro.local>
2023-04-13 04:20:19 -0400

    test

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.get('/healthz',function(req, res) {
   res.status(200).send(); 
 });
 
-app.get('/test',function(req, res) {
-  // helper.statsdClient.increment('health_counter');
-  res.status(200).send(); 
-});
+// app.get('/test',function(req, res) {
+//   // helper.statsdClient.increment('health_counter');
+//   res.status(200).send(); 
+// });
 
 app.use('/v1/user',userRoutes);
 app.use('/v1/product',productRoutes);
